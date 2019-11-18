3d2dc6bb92ed3b6b575a40af33e8d40f67056976
Furukawa(lab) <rfurukawa3@gmail.com>
2019-11-18 18:28:48 +0900

    ビット数を柔軟に設定できるよう変更 使わないオーバーロードを削除

--- fixedpoint/sample.cpp
+++ fixedpoint/sample.cpp
@@ -6,33 +6,34 @@
 using namespace std;
 
 int main() {
-	const int SFT = 12;
-
-	// fixed_pt<データ型, ビットシフト量>で宣言する
+	// fixed_pt<データ型>で宣言する
 	// データ型は全体のビット数を決める（例：int型なら32ビット）
-	// ビットシフト量は小数部のビット数を決める．
-	fixed_pt<int, SFT> a;
+	// 変数名(値，整数部ビット数，小数部ビット数）で定義
+	fixed_pt<long long> a(1.1, 9, 13), b(5.55, 12, 12);
 
-	// set()で値を設定
+	// set(値)で値を設定
 	a.set(3.14);
 
-	// get()で値をdoubleで取得
-	cout << "a = " << a.get() << endl;
+	// setbit(整数部ビット数，小数部ビット数)でビット幅を設定
+	a.setbit(12, 12);
 
-	// 宣言と同時に値を設定することもできる．
-	fixed_pt<int, SFT> b(5.55);
+	// getd()で値をdoubleで取得
+	cout << "a = " << a.getd() << endl;
 
-	// 四則演算はいつも通り
-	// 掛け算と割り算はオーバーフローしやすいので注意
-	fixed_pt<int, SFT> c;
+	// 足し算，引き算，掛け算はいつも通り
+	// 割り算はできない
+	fixed_pt<long long> c(0.0, 12, 12);
 	c = a + b;
-	cout << "3.14 + 5.55 = " << c.get() << endl;
+	cout << "3.14 + 5.55 = " << c.getd() << endl;
 	c = a - b;
-	cout << "3.14 - 5.55 = " << c.get() << endl;
-	c = a * b;
-	cout << "3.14 * 5.55 = " << c.get() << endl;
-	c = a / b;
-	cout << "3.14 / 5.55 = " << c.get() << endl;
+	cout << "3.14 - 5.55 = " << c.getd() << endl;
 
-	return 0;
+	// 掛け算ではビット数が増えるので注意
+	// 代入演算子は左右のビット幅が一致していないとエラーを吐くのでちゃんと設定する
+	c.setbit(24, 24);
+	c = a * b;
+	cout << "3.14 * 5.55 = " << c.getd() << endl;
+
+
+	return 0; 
 }
